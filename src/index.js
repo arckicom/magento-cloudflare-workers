57b90247386ca4ba98438db068fcfdfd1031cd4f
Steve Wong <43334431+arckicom@users.noreply.github.com>
2025-05-31 00:19:01 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 export default {
   async fetch(request, env, ctx) {
     // Write to KV
-    await env.MAGENTO_KV.put("690a18d714744d79b3162bff025c69b0", "This is stored in KV");
+    await env.MAGENTO_KV.put("exampleKey", "This is stored in KV");
 
     // Read from KV
-    const value = await env.MAGENTO_KV.get("690a18d714744d79b3162bff025c69b0");
+    const value = await env.MAGENTO_KV.get("exampleKey");
 
     return new Response(`KV Value: ${value}`);
   }
